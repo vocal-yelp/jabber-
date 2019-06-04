2a674a09c4420b5f5fa55fac1fd3342b7b1078cb
Peter Hart <Oliverdog4@gmail.com>
2019-06-03 19:42:03 -0500

    For Hosting withStatic

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use( express.static( `${__dirname}/../build` ) );
 
 const { SERVER_PORT } = process.env;
 
